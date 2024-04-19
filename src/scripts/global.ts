98b720572738b38a43b8f312ff8866e9538363da
Giulio Zanchetta <zank@outlook.it>
2024-04-19 19:48:42 +0200

    Refactored animation timeline handling
    The code changes involve a significant refactoring of how animation timelines are handled. The previous approach of directly creating and manipulating timelines has been replaced with a more robust system that checks for existing timelines before creation, and allows for restarting and pausing them. This change enhances the flexibility and control over animations in the application. Additionally, new utility functions have been introduced to streamline this process further.

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -4,8 +4,25 @@ export const gsapTimelines: {
 	[key: string]: gsap.core.Timeline;
 } = {};
 
-export function createTimeline(key: string) {
-	const newTimeline = gsap.timeline();
+export function createTimeline(
+	key: string,
+	timelineOptions?: gsap.TimelineVars,
+) {
+	const newTimeline = gsap.timeline(timelineOptions);
+	newTimeline.paused(true);
 	gsapTimelines[key] = newTimeline;
 	return newTimeline;
+}
+
+export function useTimeline(key: string, timelineOptions?: gsap.TimelineVars) {
+	let tl = gsapTimelines[key];
+
+	if (tl) {
+		tl.restart().then(() => tl.pause());
+	} else {
+		tl = createTimeline("to");
+		tl.play().then(() => tl.pause());
+	}
+
+	return tl;
 }
